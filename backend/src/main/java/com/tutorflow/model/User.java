9ac224fe6b636f8ccdc094225fee2b2fd4e96a3e
i549083 <i549083@fontysict.nl>
2026-03-27 10:58:26 +0100

    added User class to model

---
+++ backend/src/main/java/com/tutorflow/model/User.java
@@ -0,0 +1,82 @@
+package com.tutorflow.model;
+import java.time.LocalDateTime;
+
+public class User {
+    private Integer id;
+    private String fullName;
+    private String email;
+    private String passwordHash;
+    private UserRole role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    public User(Integer id, String fullName, String email, String passwordHash, UserRole role,
+                LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
