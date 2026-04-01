aaede8490bc1bfd04fdd57fc759481d4f08e89d7
i549083 <i549083@fontysict.nl>
2026-04-01 20:19:39 +0200

    added UserResponseDTO to make normal api so its gonna return JSON

--- backend/src/main/java/com/tutorflow/controller/UserController.java
+++ backend/src/main/java/com/tutorflow/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.tutorflow.controller;
 
+import com.tutorflow.dto.UserResponseDTO;
+import com.tutorflow.model.User;
+import com.tutorflow.repository.IUserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final IUserRepository userRepository;
+
+    public UserController(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/me")
-    public String getCurrentUser(Authentication authentication) {
-        return "Authenticated as: " + authentication.getName();
+    public UserResponseDTO getCurrentUser(Authentication authentication) {
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email);
+
+        return new UserResponseDTO(
+                user.getId(),
+                user.getFullName(),
+                user.getEmail(),
+                user.getRole().name()
+        );
     }
 }
